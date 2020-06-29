77113c283384bd1a31be9d14906efe6288aeb732
Enrique B Montas <enriquem@fb.com>
2020-06-29 12:07:21 -0400

    review comments

--- app/src/main/java/com/example/flixter/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixter/MovieDetailsActivity.java
@@ -2,16 +2,22 @@ package com.example.flixter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixter.databinding.ActivityMovieDetailsBinding;
 import com.example.flixter.models.Movie;
 
 import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -20,6 +26,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvTitle;
     TextView tvOverview;
     RatingBar rbVoteAverage;
+    ImageView ivPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +39,29 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = binding.tvTitle;
         tvOverview = binding.tvOverview;
         rbVoteAverage = binding.rbVoteAverage;
+        ivPoster = binding.ivPoster;
+
 
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
+        Context context = getApplicationContext();
+
+        boolean portrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        String imageUrl = portrait ? movie.getPosterPath() : movie.getBackdropPath();
+        int placeholder = portrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+
+        final int radius = 30; // corner radius, higher value = more rounded
+        final int margin = 10; // crop margin, set to 0 for corners with no crop
+
+
+        Glide.with(context).load(imageUrl)
+                .placeholder(placeholder)
+                .error(placeholder)
+                .transform(new RoundedCornersTransformation(radius, margin))
+                .into(ivPoster);
 
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
